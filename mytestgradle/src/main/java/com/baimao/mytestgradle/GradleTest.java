d6ead5f58244f6df76d49321004413fe0de806ee
昊月 <androidmao@163.com>
2017-11-26 18:25:25 +0800

    1，初始化导入；

---
+++ mytestgradle/src/main/java/com/baimao/mytestgradle/GradleTest.java
@@ -0,0 +1,14 @@
+package com.baimao.mytestgradle;
+
+import android.util.Log;
+
+/**
+ * Created by baimao on 2017/11/26.
+ */
+
+public class GradleTest {
+
+    public void test() {
+        Log.i("GradleTag", "GradleTest");
+    }
+}
